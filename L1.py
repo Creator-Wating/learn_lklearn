505a8657790e4db3a84f8de5a9852020be3e864f
zzb <zzb26262@gmail.com>
2021-11-24 16:34:10 +0800

    学习机械学习第一张

---
+++ L1.py
@@ -0,0 +1,10 @@
+import numpy as np
+import os
+import pandas as pd
+import tarfile
+from six.moves import urllib
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+HOUSING_PATH = os.path.join("datasets", "housing")
+HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
